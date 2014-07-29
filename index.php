79f332936ea5f3736427f2120a256ce37595c7cd
William <william.l.danielsson@gmail.com>
2014-07-29 12:29:18 +0200

    Divided into php-files

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include_once "header.php"; ?>
+<!--
+<body>
+	<div class="content">
+-->
+		<div class="pagecontent">
+			<div class="register">
+			</div>
+
+			<div class="image">
+			</div>
+		</div>
+	</div>
+<!--
+	<h1>SIGNUP</h1>
+	<form id="register" action="register.php" method="get" accept-charset="UTF-8">
+		<input type="email" name="email" id="email" maxlength="50" />
+
+		<input type="password" name="password" id="password" maxlength="50" />
+
+		<input type="submit" name="Submit" value="Submit" />
+
+	</form>
+
+	<h1>LOGIN</h1>
+	<form id="login" action="login.php" method="get" accept-charset="UTF-8">
+		<input type="email" name="email" id="email" maxlength="50" />
+
+		<input type="password" name="password" id="password" maxlength="50" />
+
+		<input type="submit" name="Submit" value="Submit" />
+
+	</form>
+
+-->
+
+</body>
+
+</html>
